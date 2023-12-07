b1d8e3f213f508379d12bf75be04e86037697490
Citosh <chaplinskyartem@gmail.com>
2023-12-07 11:15:22 +0000

    feat(loading animation) added component

--- src/components/loading-animation/loading-animation.jsx
+++ src/components/loading-animation/loading-animation.jsx
@@ -1,15 +1,42 @@
-import { component$, useStore } from '@builder.io/qwik';
-import { QwikLottie } from 'qwik-lottie';
-import AnimationData from '../../../public/Loading-animation.json'
+import { useStore, useStyles$ } from '@builder.io/qwik';
+
+import TestStyles from './test.css?inline'
+import { component$, useVisibleTask$, useSignal, noSerialize } from '@builder.io/qwik';
+import lottie from 'lottie-web';
+
+function initLoopedSegment(animation, startFrame, endFrame) {
+  animation.addEventListener('drawnFrame', (event) => {
+    const frame = Math.round(event.currentTime)
+    if (frame >= endFrame) {        
+      animation.setDirection(-1)
+    } else if (frame == startFrame) {        
+      animation.setDirection(1)
+    }
+  })
+}
 
 export default component$(() => {
-  // const defaultOptions  = useStore({
-  //   animationData: AnimationData      
-  //     //path: '../../../public/Loading-animation.json',
-  // });
+  useStyles$(TestStyles)
+
+  const store = useStore({
+    anim: noSerialize({}),
+  });
+  const canvas = useSignal();  
+  useVisibleTask$(() => {
+    let animation = store.anim = noSerialize(
+      lottie.loadAnimation({
+        container: canvas.value,
+        loop: false,
+        autoplay: true,
+        path: '/Loading-animation.json',
+
+      })
+    );
+
+    initLoopedSegment(animation, 36, 100)
+  });
+
   return (
-      <div>
-          <QwikLottie animationData={AnimationData} />
-      </div>
+    <div class='box' ref={canvas}></div>
   );
 });
