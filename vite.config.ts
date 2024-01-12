f6e568f8436f6ef755c25b64f7e60eca543e42db
Igor Yanishevskyi <iianishevskyi@skiff.com>
2024-01-12 22:28:57 +0000

    build(vite): run polling if in WSL only, update deps

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,14 @@
 import { defineConfig } from "vite";
 import { qwikVite } from "@builder.io/qwik/optimizer";
 import { qwikCity } from "@builder.io/qwik-city/vite";
-import { builderDevTools } from "@builder.io/dev-tools/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { qwikReact } from "@builder.io/qwik-react/vite";
+import isWsl from 'is-wsl';
 
 export default defineConfig(({ mode }) => {
   const isDevelopmentMode = mode === 'development';
   return {
     plugins: [
-      builderDevTools(),
       qwikCity(),
       qwikVite(),
       tsconfigPaths(),
@@ -17,7 +16,7 @@ export default defineConfig(({ mode }) => {
     ],
     server: {
       watch: {
-        usePolling: true, // we run inside WLS
+        usePolling: isWsl, // use polling if we run inside WSL
         interval: 2000,
         binaryInterval: 5000,
       }
